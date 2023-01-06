b1b1ad201926bed844ec6df95c19491d5d76d020
Peter <pete.blinov@yandex.ru>
2023-01-06 19:16:56 +0300

    chore: play colors

--- app.vue
+++ app.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <NuxtPage class="page  min-h-full" />
+  <!-- <div class="bg-light-300 min-h-full">
+  </div> -->
 </template>
+
+<style>
+html, body, #__nuxt, .page {
+  min-height: 100%;
+  height: 100%;
+}
+
+/* ----------- colors --------- */
+
+:root {
+  --c-section-border: hsl(0deg 0% 96%);
+}
+
+.selected {
+  /* background-color: bisque; */
+  background-color: hsl(0deg 100% 84%);
+}
+
+.section-tags {
+  background-color: hsl(315deg 22% 74%);
+  background-color: hsl(315deg 18% 67%);
+  
+  background-color: cadetblue;
+  
+  background-color: hsl(212deg 15% 83%);
+  background-color: hsl(212deg 16% 80%);
+
+  border-bottom: 3px solid var(--c-section-border);
+}
+
+.message {
+  background-color: var(--c-section-border);
+}
+
+/* .card {
+  @apply border-2 border-transparent;
+} */
+
+.card:hover {
+  background-color: hsl(260deg 17% 93%);
+  background-color: hsl(0deg 0% 98%);
+  
+  /* background-color: hsl(216deg 14% 95%); */
+  /* border: 2px solid hsl(216deg 14% 90%); */
+  /* @apply bg-light-600 border-light-900; */
+  
+}
+
+html {
+  background-color: hsl(266deg 16% 82%);
+  
+  @apply bg-light-700;
+}
+
+</style>
