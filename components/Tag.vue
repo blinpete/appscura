4a8867d096a76250a1812a3dcf15f1ba0e0a8e56
Peter <pete.blinov@yandex.ru>
2023-01-06 19:16:33 +0300

    chore: update tag

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,8 +1,8 @@
 <template>
-  <button class="bg-light-400 rounded-md px-2 text-cool-gray-700 relative">
+  <button class="tag bg-light-400 rounded-md rounded-tr-none px-2 text-cool-gray-700 relative">
     <slot />
 
-    <div v-if="counter || close" class="absolute -top-2 -right-1 bg-blue-100 text-dark-300 rounded-full z-10 w-4 h-4 text-xs align-middle text-center">
+    <div v-if="counter || close" class="tag__dot absolute -top-2.2 -right-2 text-dark-300 rounded-full z-10 w-4 h-4 text-xs align-middle text-center">
       <span v-if="counter !== undefined">{{ counter }}</span>
       <span v-if="close">&times;</span>
     </div>
@@ -15,3 +15,13 @@ defineProps<{
   close?: boolean
 }>()
 </script>
+
+<style>
+.tag:hover {
+  transform: scale(1.1);
+  transition: all 0.1s ease-out;
+}
+.tag__dot {
+  background-color: inherit;
+}
+</style>
