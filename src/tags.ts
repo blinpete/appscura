80bed781df0647357dd16567c427b261464e617d
Peter <pete.blinov@yandex.ru>
2023-01-07 03:34:40 +0300

    chore: new tags

--- src/tags.ts
+++ src/tags.ts
@@ -1,13 +1,25 @@
 export const tags = [
-  'markdown',
-  'zettelkasten',
-  'outliner',
+  'zettelkasten', // evegreen note, digital garden, second brain
+
+  'note-taking',
+
+  'outliner', // bullet journal, bullet list
+  'kanban',
+  'todo',
 
   'free',
   'paid',
 
   'local',
   'server',
+
+  'collab', // colaboration, for teams
+
+  'markdown',
+  'mindmap',
+  'visual', // visual thinking
+
+
 ] as const
 
 export type Tag = typeof tags[number]
