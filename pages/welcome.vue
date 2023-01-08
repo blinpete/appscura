def067d58ca9866a746a532029c9e73d6b899a2f
Peter <pete.blinov@yandex.ru>
2023-01-08 14:45:37 +0300

    feat: welcome page

---
+++ pages/welcome.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="bg-light-900 flex">
+    <Collage />
+
+    
+    <div class="m-auto text-center z-10">
+      <div class="flex justify-center">
+        <img src="/favicon-2.png" alt="logo" width="60" height="60">
+      </div>
+
+      <div class="text-6xl font-bold mb-4">
+        <!-- <h1>Appsurd</h1> -->
+        <h1>Appscura</h1>
+        <!-- <h1>Appscurus</h1> -->
+        <!-- <h1>Appscurio</h1> -->
+        <!-- <h1>Appscurious</h1> -->
+      </div>
+      <h2>Community-driven curated collection of anything you may need to stay organized</h2>
+
+  
+      <!-- <p>Looking for an app to take smarter notes, start building your personal knowledge base or a place for your day-to-day TODOs?</p> -->
+  
+      <BaseButton to="/" label="Dive in" class="mt-8" />
+    </div>
+  </main>
+</template>
